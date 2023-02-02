5b5365c3067028f8ece4351d7a7c77d527cce276
meonghiem <meonghiem@gmail.com>
2023-02-02 06:42:38 +0000

    add_list_review_screen

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -8,7 +8,6 @@ class BookReviewsController < ApplicationController
 
   # GET /book_reviews/1 or /book_reviews/1.json
   def show
-      @book_review.user_id = current_user.user_id
   end
 
   # GET /book_reviews/new
